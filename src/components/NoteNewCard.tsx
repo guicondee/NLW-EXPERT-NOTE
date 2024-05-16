627c88ba7fd2cebedb3725db8a217bb2891ebddb
Guilherme vinicios conde <guicondee@outlook.com>
2024-05-16 15:22:49 -0300

    adicionado API speechRecognition para gravaçao de audio do usuario, convertendo em texto no textarea, e ajustando responsividade nos dispositivos menores

--- src/components/NoteNewCard.tsx
+++ src/components/NoteNewCard.tsx
@@ -1,10 +1,14 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
-import { ChangeEvent, FormEvent, FormEventHandler, useState } from 'react'
-import { Toaster, toast } from 'sonner'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { toast } from 'sonner'
+
 export type NewNoteCardProps = {
   onNoteCreated: (content: string) => void
 }
+
+let speechRecognition: SpeechRecognition | null = null
+
 export function NoteNewCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
   const [content, setContent] = useState('')
@@ -35,16 +39,53 @@ export function NoteNewCard({ onNoteCreated }: NewNoteCardProps) {
   }
 
   const handleStartRecord = () => {
+    const isSpeechRecognitionApiAvailable =
+      'speechRecognition' in window || 'webkitSpeechRecognition' in window
+
+    if (!isSpeechRecognitionApiAvailable) {
+      alert('Infelizmente seu navegador não suporta a API de gravação.')
+      return
+    }
+
     setIsRecording(true)
+    setShouldShowOnboarding(false)
+
+    const SpeechRecognitionAPI =
+      window.SpeechRecognition || window.webkitSpeechRecognition
+
+    speechRecognition = new SpeechRecognitionAPI()
+
+    speechRecognition.lang = 'pt-BR'
+    speechRecognition.continuous = true
+    speechRecognition.maxAlternatives = 1
+    speechRecognition.interimResults = true
+
+    speechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript)
+      }, '')
+
+      setContent(transcription)
+    }
+
+    speechRecognition.onerror = (event) => {
+      console.error(event)
+    }
+
+    speechRecognition.start()
   }
 
   const handleStopRecording = () => {
     setIsRecording(false)
+
+    if (speechRecognition !== null) {
+      speechRecognition?.stop()
+    }
   }
 
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="outline-none focus-visible:ring-2 focus-visible:ring-lime-400 hover:ring-2 hover:ring-slate-600 flex flex-col rounded-md bg-slate-700 p-5 gap-3 text-left">
+      <Dialog.Trigger className="outline-none  focus-visible:ring-2 focus-visible:ring-lime-400 hover:ring-2 hover:ring-slate-600 flex flex-col rounded-md bg-slate-700 p-5 gap-3 text-left">
         <span className="text-sm font-medium text-slate-200">
           Adicionar Nota
         </span>
@@ -58,7 +99,7 @@ export function NoteNewCard({ onNoteCreated }: NewNoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
 
-        <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 outline-none rounded-md flex flex-col">
+        <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:max-w-[640px] max-w-[380px] w-full h-[40vh] lg:h-[60vh] bg-slate-700 outline-none rounded-md flex flex-col">
           <Dialog.DialogClose className="absolute right-0 top-0 bg-slate-800 p-[6px] text-slate-400">
             <X className="size-5 hover:text-slate-100" />
           </Dialog.DialogClose>
